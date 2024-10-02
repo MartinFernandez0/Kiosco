b12b3c90a736c217dbeffeaa873e47b2e7faf943
MartinFernandez0 <martinexefe@gmail.com>
2024-10-02 17:59:41 -0300

    ajuste personalizado

--- KioscoInformaticoWeb/Program.cs
+++ KioscoInformaticoWeb/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using KioscoInformaticoWeb;
 using KioscoInformaticoServices.Interfaces;
 using KioscoInformaticoServices.Services;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
